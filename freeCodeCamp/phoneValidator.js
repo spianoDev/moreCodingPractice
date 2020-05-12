c2bee7a2415ca418482b4868f906f4745451f19e
spiano <lafleurpiano@gmail.com>
2020-05-11 22:05:28 -0500

    phone project day2

--- freeCodeCamp/phoneValidator.js
+++ freeCodeCamp/phoneValidator.js
@@ -14,33 +14,51 @@
 //
 function telephoneCheck(str) {
     let numCount = 0;
+    let parenthesis = (str.indexOf(')')) - str.indexOf('(');
+    console.log(parenthesis);
     if (str.length < 10 || str.length > 16) {
         console.log('not a valid US phone number');
         return false;
-    } for (let char of str) {
+    }
+    for (let char of str) {
         if (!char.search(/[^0-9-\s\(\)]/)) {
             console.log('not a real number');
             return false;
-    } else {
-            if (str[1] === ' ' || str[1] === '(' && str[0] !== "1") {
-                console.log('not the right country code');
+        } else if (char.search(/[^0-9]/)) {
+            numCount += 1;
+            console.log(numCount);
+            if (str[0] !== "1" && numCount === 11) {
+                console.log('its not valid');
                 return false;
-            } else if (char.search(/[^0-9]/)) {
-                numCount += 1;
-                console.log(numCount);
-                if (numCount === 10 || numCount === 11 && str[0] === "1") {
-                    console.log('its valid');
-                }
-
-
             }
         }
     }
-
+    if (numCount === 10) {
+        if (parenthesis !== 4 && str.indexOf('(') === -1) {
+            console.log('not grouped correctly');
+            return false;
+        } else if (str[1] === ' ' || str[1] === '(' && str[0] !== "1") {
+            console.log('not the right country code');
+            return false;
+        } else if (parenthesis === 4 && str.indexOf('(') === -1) {
+            console.log('so close...');
+            return false;
+        } else if (parenthesis !== 4 && str.indexOf('(') !== -1) {
+            console.log('another poor grouping');
+            return false;
+        }
+        if (str.indexOf('(') === -1 && str.indexOf(')') === -1) {
+            console.log('this is the one');
+            return true;
+        } else if (numCount === 11 && str[0] === '1') {
+            console.log('found one!');
+            return true;
+        }
+    }
 }
 
-// telephoneCheck("555-555-5555"); // should return true
-telephoneCheck("1 555-555-5555"); // should return true.
+telephoneCheck("555-555-5555"); // should return true
+// telephoneCheck("1 555-555-5555"); // should return true.
 // telephoneCheck("1(555)555-5555"); // should return true.
 // telephoneCheck("10 (757) 622-7382"); // should return false.
 // telephoneCheck("123**&!!asdf#"); // should return false.
@@ -48,3 +66,7 @@ telephoneCheck("1 555-555-5555"); // should return true.
 // telephoneCheck("1 (555) 555-5555");// should return true.
 // telephoneCheck("2 (757) 622-7382"); // should return false.
 // telephoneCheck("2 (757) 622-7382"); // should return false.
+// telephoneCheck("(6054756961)"); // should return false
+// telephoneCheck("(555-555-5555"); // should return false.
+// telephoneCheck("555)-555-5555"); // should return false.
+// telephoneCheck("(275)76227382"); // should return false.
